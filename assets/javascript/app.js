094d0ee70200a774bc7e9c583a3ff924201e48e0
Bhanoo Gulati <bhanoo.gulati@gmail.com>
2017-12-25 13:35:10 -0500

    more code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,30 +1,18 @@
 
-/*
-for all questions in the questions  bank 
-// Reset Timer 
-1. Display Question
-2. Display Answers
-3. Start timer 
-4. If Time Up or If Done button Clicked 
--> Stop Timer  & Clear Timer 
--> Check if the answer selected is correct 
-- > Calculate score
 
-if counter is greater than length then display score 
-
-*/
-var maxTime = 5; 
+var maxTime = 7; 
 var counter = 0;
 var timeLeft = maxTime; 
-var timeUp = false; 
-var doneClicked = false ; 
-var timeStarted = false; 
+//var timeUp = false; 
+//var doneClicked = false ; 
+//var timeStarted = false; 
 var interval ; 
 var selectedValue; 
 var correctOption; 
 var selectedId ; 
 var wrongs = 0 ; 
 var rights = 0 ; 
+var rightAnswer = false; 
 
 var questionBank = [
 { Q:"How long a memory do Goldfish have", A1: "3 second", A2: "5 mins", A3: "3 weeks", A4: "5 months"},
@@ -34,40 +22,57 @@ var questionBank = [
 var answerBank = ["5 months", "Ohio", "A Blessing"];
 
 function resetTimer(){
+	timeLeft = maxTime; 
 	$('#displayTimer').val(maxTime);
 }
 
 function startTimer(){
-	timeStarted = true; 
-	timeUp = false; 
+	//timeStarted = true; 
+	//timeUp = false; 
 	interval = setInterval(displayTimeLeft, 1000); 
 
 }
 
 function stopTimer(){
 	clearInterval(interval); 
-	timeStarted = false; 
+	//timeStarted = false; 
 
 }
 
 function displayTimeLeft(){
 	$('#displayTimer').val(timeLeft); 
 	timeLeft --	
-	if(timeLeft < 0 )
-		{
-			timeUp = true; 
+	if(timeLeft < 0 ){
+			//timeUp = true; 
 			$('#displayTimer').val('Time Out !'); 
-			stopTimer(); 
-		}
+			stopTimer();
+			//validate answer  
+			rightAnswer = validateAnswer(); 
+			//Update score 
+			updateScore(rightAnswer); 
+			//Increase counter
+			counter++; 
+			//wait for 3 seconds and display the next question 
+			if(counter< questionBank.length){
+				setTimeout(displayQnA, 3000, counter ); 
+			}
+			else
+			document.write('wrongs:' + wrongs + 'rights:' + rights ); 
+	}
+	
 }
 
 function displayQnA(counter){
+	
 	resetTimer(); 
+	$('input[name=Answer]:radio').prop('checked', false); 
 	$('#displayQuestion').val(questionBank[counter].Q); 
 	$('#Option1').next().text(questionBank[counter].A1); 
 	$('#Option2').next().text(questionBank[counter].A2); 
 	$('#Option3').next().text(questionBank[counter].A3); 
 	$('#Option4').next().text(questionBank[counter].A4); 
+
+	startTimer(); 
 }
 
 function correctAnswer(correctAnswer){
@@ -76,10 +81,11 @@ function correctAnswer(correctAnswer){
 
 function validateAnswer(){
 	
-	//if no radio is selected mark as incorrect 
+	//if no radio is selected mark as incorrect 	
 	for(var i = 0; i< $('input[name=Answer]:radio').length; i ++){
 		//if radio is selected 
 		if ($('input[name=Answer]:radio')[i].checked){
+			checked = true ; 
 			//get the radio id option selected 
 			selectedId = '#'+ $('input[name=Answer]:radio')[i].id
 			//get the label text of that radio id
@@ -88,40 +94,41 @@ function validateAnswer(){
 			correctOption = answerBank.find(correctAnswer) ; 
 			//if yes , update score 
 			if(selectedValue == correctOption){
-				console.log('Yay!'); 
-				rights += 1; 
+				rightAnswer = true; 
+				return rightAnswer; 
 			}
-			else if(selectedValue != correctOption){
-				// update score 
-				console.log('Nay !') ;
-				wrongs += 1;
-			}
-
+			
 		}
-
-		else
-			console.log('Nay !') ;
-			wrongs += 1;
-
+		
 	}
 
 }
 
+function updateScore(rightAnswer){
+	if (rightAnswer){
+		console.log('Yay!'); 
+		rights += 1;
+	}
+	else
+		console.log('Nay!'); 
+		wrongs += 1;
+}
+
 //Call the required functions
 displayQnA(counter); 
-startTimer(); 
+
 $('#doneButton').on('click', function(){
 	//stop timer 
 	stopTimer();  
-	//validate answer  & Update Score 
-	validateAnswer(); 
+	//validate answer  
+	rightAnswer = validateAnswer(); 
+	//Update score 
+	updateScore(rightAnswer); 
 	//increase counter 
 	counter++; 
-	//wait for 2 seconds and display the next question 
-	if(counter< displayQuestion.length){
-		setTimeout(displayQnA(counter), 2000); 
-		timeLeft = maxTime; 
-		startTimer(); 
+	//wait for 3 seconds and display the next question 
+	if(counter< questionBank.length){
+		setTimeout(displayQnA, 3000, counter ); 
 	}
 	else
 		document.write('wrongs:' + wrongs + 'rights:' + rights ); 
